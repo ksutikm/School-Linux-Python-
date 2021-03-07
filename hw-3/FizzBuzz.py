26d518bbb6b3f610ecead9b16093a558e5a8b78a
ksutikm <tsksusha@yandex.ru>
2021-03-08 00:01:09 +0300

    Solve FizzBuzz

---
+++ hw-3/FizzBuzz.py
@@ -0,0 +1,28 @@
+"""
+Напишите программу, которая выводит на экран числа от 0 до 100.
+При этом вместо чисел, кратных трем, программа должна выводить слово «Fizz»,
+а вместо чисел, кратных пяти — слово «Buzz». Если число кратно и 3, и 5,
+то программа должна выводить слово «FizzBuzz»
+"""
+def way_1():
+    for i in range(101):
+        print('FizzBuzz' if i % 15 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i)
+
+def way_2():
+    list = []
+    for i in range(101):
+        list.append('FizzBuzz' if i % 15 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i)
+    print(list)
+
+def way_3():
+    for i in range(101):
+        n, f, b = str(i), 'Fizz', 'Buzz'
+        if i%3!=0: f=''
+        else: n=''
+        if i%5!=0: b=''
+        else: n=''
+        print(n+f+b)
+
+# way_1()
+# way_2()
+way_3()
