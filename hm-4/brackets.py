e65ab0e89ae59e0d430f9c604c1fbfdc54ca339c
ksutikm <tsksusha@yandex.ru>
2021-03-11 14:36:51 +0300

    Solve the task about brackets

---
+++ hm-4/brackets.py
@@ -0,0 +1,19 @@
+def check_braclets(s):
+    open_brackets = ['(', '{', '[']
+    close_brackets = [')', '}', ']']
+    dictionary = {')': '(', '}': '{', ']': '['}
+    stack = []
+    for c in s:
+        if c in open_brackets:
+            stack.append(c)
+        elif c in close_brackets:
+            if len(stack) <= 0 or stack.pop() != dictionary[c]:
+                return 'Неправильное количество скобок!'
+
+    if len(stack) == 0:
+        return 'Все скобки раставлены верно!'
+    else:
+        return'Неправильное количество скобок!'
+
+s = input('Введите строку: ')
+print(check_braclets(s))
